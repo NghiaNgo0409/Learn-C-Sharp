1dbb0876d9e4ccc1091aedb5ddead396081e831d
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2022-04-02 12:02:07 +0700

    Variables

--- Program.cs
+++ Program.cs
@@ -7,8 +7,17 @@ namespace LearnCode
         static void Main(string[] args)
         {
             Console.Title = "AI Bot";
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
 
-            Console.WriteLine("Hello World!");
+            double num1, num2, num3, average;
+            Console.WriteLine("Input number 1: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Input number 2: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Input number 3: ");
+            num3 = Convert.ToDouble(Console.ReadLine());
+            average = (num1 + num2 + num3) / 3;
+            Console.WriteLine("The average of three number is " + average);
 
             Console.ReadKey();
         }
